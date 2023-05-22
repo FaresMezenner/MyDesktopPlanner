9629d5a335494e72070480b7a614fd2621777916
Soapiane <Ls_yekene@esi.dz>
2023-05-22 18:56:17 +0200

    Added creneau right side bar

---
+++ src/main/java/com/example/mydesktopplanner/View/RightSidePannelCreneauController.java
@@ -0,0 +1,29 @@
+package com.example.mydesktopplanner.View;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RightSidePannelCreneauController implements Initializable {
+
+    @FXML
+    ChoiceBox<String> CategorieChoiceBox;
+    @FXML
+    ChoiceBox<String> PrioriteChoiceBox;
+    @FXML
+    ChoiceBox<String> EtatChoiceBox;
+
+    private String[] categories = {"Studies", "Work", "Hobby","Sport","Health","Other"};
+    private String[] priorites = {"Low", "Medium", "High"};
+    private String[] Etat = {"Not realized", "Completed", "In progress","Canceled","Delayed","Unscheduled"};
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        CategorieChoiceBox.getItems().addAll(categories);
+        PrioriteChoiceBox.getItems().addAll(priorites);
+        EtatChoiceBox.getItems().addAll(Etat);
+    }
+}
