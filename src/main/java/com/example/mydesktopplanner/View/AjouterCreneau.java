278da68e6bb6b9951da077dfebc048d5e5533c8b
Soapiane <Ls_yekene@esi.dz>
2023-05-22 22:32:28 +0200

    Added AjouterCreneau

--- src/main/java/com/example/mydesktopplanner/View/AjouterCreneau.java
+++ src/main/java/com/example/mydesktopplanner/View/AjouterCreneau.java
@@ -62,7 +62,6 @@ public class AjouterCreneau implements Initializable {
                 }
             }
         });
-
     }
 
 
---
+++ src/main/java/com/example/mydesktopplanner/View/AjouterPeriode.java
@@ -0,0 +1,52 @@
+package com.example.mydesktopplanner.View;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AjouterPeriode implements Initializable {
+
+    @FXML
+    ChoiceBox<String> CategorieChoiceBox;
+    @FXML
+    ChoiceBox<String> PrioriteChoiceBox;
+
+    @FXML
+    TextField PeriodiciteTextField;
+
+    @FXML
+    TextField NbPlannificationsTextField;
+
+    @FXML
+    TextField DureeTextField;
+
+
+
+    private String[] categories = {"Studies", "Work", "Hobby","Sport","Health","Other"};
+    private String[] priorites = {"Low", "Medium", "High"};
+    private String[] Etat = {"Not realized", "Completed", "In progress","Canceled","Delayed","Unscheduled"};
+
+    public void setNumeric(TextField tf){
+        tf.textProperty().addListener((observable, oldValue, newValue) -> {
+            // Vérifie chaque caractère entré
+            if (!newValue.matches("\\d*")) {
+                tf.setText(newValue.replaceAll("[^\\d]", ""));
+            }
+    });}
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        CategorieChoiceBox.getItems().addAll(categories);
+        PrioriteChoiceBox.getItems().addAll(priorites);
+
+        // Ajoute un écouteur d'événements pour le TextField
+        setNumeric(NbPlannificationsTextField);
+        setNumeric(DureeTextField);
+        setNumeric(PeriodiciteTextField);
+    }
+
+}
